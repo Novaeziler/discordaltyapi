49e7338baf456be916687774148325731a71dd5d
Glitch (tricky-exclusive-suggestion) <none>
2020-11-21 17:55:17 +0000

    NOVAEZİLER

---
+++ komutlar/davet.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, params) => {
+  if (!params[0]) {
+    const commandNames = Array.from(client.commands.keys());
+	message.channel.send({embed: {
+            color: 0xD97634,
+            author: {
+              name: "Davet Menüsü",
+              icon_url: "https://cdn.discordapp.com/avatars/495214808484806657/37b2db3f7519e024d25521ac272f7a0d.png?size=2048"
+            },
+			    "thumbnail": {
+				 "url": "https://cdn.discordapp.com/avatars/495214808484806657/37b2db3f7519e024d25521ac272f7a0d.png?size=2048"
+			},
+            title: "",
+            description: "[Davet Linkim](https://discord.com/oauth2/authorize?client_id=776423230738268182&scope=bot&permissions=805829694) \n[Destek Sunucusu](https://discord.gg/FYm8aba)",
+            fields: [
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: "",
+              text: "© ₺urksh © "
+            }
+          }
+        });
+	    message.react("📝")
+}};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['d', 'link', 'linkler'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'davet',
+  description: '',
+  usage: 'davet'
+};
